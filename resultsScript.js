7deab064d727fca777adca97c5de6b6f66d37eb7
ajdorenk <Austin@ajdmbpro.home>
2013-10-09 00:02:57 -0500

    More minor additions

---
+++ resultsScript.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+
+	var $item1Arr = {itemName: 'Discrete Mathematics and with Applications', 
+		itemDescripShort: "Used for Cpr E 310", itemSeller: 'Austin Dorenkamp', 
+		itemPrice: 90};
+
+		document.getElementById('itemName').innerHTML='Print this';
+
+	// for(key in $item1Arr) {
+	// 	document.getElementById(key).innerHTML=item1Arr[key];
+	// 	alert($item1Arr[key]);
+	// }
+
+})
