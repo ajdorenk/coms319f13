5b52d92e54363039fe6a1113ab154475ca303005
ajdorenk <Austin@ajdpro.local>
2013-10-10 21:57:43 -0500

    Trying to get popup to work on hover

--- resultsScript.js
+++ resultsScript.js
@@ -4,7 +4,33 @@ $(document).ready(function() {
 		itemDescripShort: "Used for Cpr E 310", itemSeller: 'Austin Dorenkamp', 
 		itemPrice: 90};
 
-		document.getElementById('itemName').innerHTML='Print this';
+	$itemName = $('.itemName');
+	if($item1Arr['itemName'].length > 15) {
+		$itemName.html($item1Arr['itemName'].substring(0,15) + "...");
+	} else {
+		$itemName.html($item1Arr['itemName']);
+	}
+
+	$itemName.hover(
+		function(e) {
+			$('.itemNamePopup').show();
+		},
+		function(e) {
+			$('itenNamePopup').hide();
+		}
+	);
+	// $itemName.mouseover(function() {
+	// 	alert('mouseover');
+	// 	$('.itemNamePopup').css({'display': 'block'});
+	// });
+	// $itemName.mouseleave(function() {
+	// 	// alert('mouseout');
+	// 	$('.itemNamePopup').style.display = 'none';
+	// });
+
+	// $('.itemName').html("Print this");
+
+	// document.getElementById('itemName').innerHTML='Print this';
 
 	// for(key in $item1Arr) {
 	// 	document.getElementById(key).innerHTML=item1Arr[key];
