85cd2f8fd1eb5ae32c4c87fdedebe909d070f3fd
ajdorenk <Austin@b01-1.nat.iastate.edu>
2013-12-08 23:54:45 -0600

    remove alert

--- Production/myaccount_files/sellerfunctions.js
+++ Production/myaccount_files/sellerfunctions.js
@@ -40,7 +40,6 @@ function getBooks(email, print)
 				}*/
 	}).done( function( data ) {
 		var books = $.parseJSON(data);
-		alert(data);
 		$.each(books,  function() {
 			arrIds[i] = this["ID"];
 			//toRet += printBook(this["ImageLink"], this["Title"], this["Author"], this["ISBN"], this["ItemShape"], this["Price"], this['Description'], this['Sold']);
